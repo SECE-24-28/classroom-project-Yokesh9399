95f69ac52172e687eca326ca96d9cb34031fa33e
Yokesh9399 <yokesh.g2024cse@sece.ac.in>
2025-12-09 16:00:35 +0530

    Add files via upload
    day 7

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,9 @@
-import React from 'react'
-
-const Header = () => {
-    let des={background:"black",color:"white"};
-    return (
-    <div>
-        <h1 style={des}>Tiger</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-            Quo architecto, eos ullam labore vel tempora voluptatem fuga! 
-            Odit eaque magni velit architecto, neque temporibus aliquid, cumque 
-            dolorem, molestias amet impedit aperiam harum iure ullam pariatur explicabo
-                assumenda deserunt porro? Temporibus iste minus ratione nemo dolorem cum nam,
-                corporis obcaecati vel aliquam, dignissimos aspernatur nulla sint consequatur
-                perferendis. Dolorem, ea nesciunt?
-        </p>
-    </div>
-    )
+function Header({ title }) {
+  return (
+    <header className="header">
+      <h1>{title}</h1>
+    </header>
+  )
 }
-export default  Header
+
+export default Header
