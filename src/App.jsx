c49a0f32e44d9fc6aa5dcb020db34ce3388d24bb
Yokesh9399 <yokesh.g2024cse@sece.ac.in>
2025-12-11 15:09:52 +0530

    myapp/feedback

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,66 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import { format } from 'date-fns'
+import api from './api/Post'
 import './App.css'
-import One from './one'
-import Two from './two'
-import Three from './three'
-import DataProvider from './datacontext'
+import Home from './home'
+import Search from './search'
+import AddPost from './AddPost'
+
 function App() {
-  return (
+  const [posts, setPosts] = useState([])
+  const [search,setSearch]=useState("")
+
+  const [title,setTitle]=useState('')
+  const [body,setBody]=useState('')
+
+
+  const handleSubmit=(e)=>{
+    e.preventDefault()
+    const id=(posts.length)?(Number(posts[posts.length-1].id)+1):(1)
+    const dateTime=format(new Date(),'MMM dd,yyyy pp')
+
+    //newObj={id:id,title:title,dateTime:datetime,body:body}
+    const newObj={id,title,dateTime,body}
+
+    api.post("/feedback",newObj)
+    const newList=[...posts,newObj]
+    setPosts(newList)
+
+    setTitle('')
+    setBody('')
+  }
+
+  useEffect(()=>
+  {
+    const fetData=async()=>{
+      try{
+      const res= await api.get('/feedback')
+      //console.log(res.data)
+      setPosts(res.data)}
+      catch(err){
+        console.log(err)
+      }
+    }
+    fetData();
+  },[])
+
+
+  const [searchResult,setSearchResult]=useState([])
+
+  useEffect(()=>
+  {
+    const filtered=posts.filter((post)=> (post.title).includes(search))
+  
+    // eslint-disable-next-line react-hooks/set-state-in-effect
+    setSearchResult(filtered)
+  },[posts,search]
+  )
+
+  return(
     <>
-     <DataProvider>
-          <One/>
-          <Two/>
-          <Three/>
-     </DataProvider>
+    <Search search={search} setSearch={setSearch}/>
+    <AddPost title={title} setTitle={setTitle} body={body} setBody={setBody} handleSubmit={handleSubmit}/>
+    <Home searchResult={searchResult}/>
     </>
   )
 }
